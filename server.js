508567cb318b6ac6537decc1d3eb26dcf2d0e2fd
Jesse Aldana <jesse.aldana@gmail.com>
2021-02-17 16:31:25 -0800

    fixed typo in server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/newt_Controller.js");
+var routes = require("./controllers/newt_controller.js");
 
 app.use(routes);
 
